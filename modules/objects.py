ec92c54b2847bd7950e8ae9e0985677ec6fe0b6f
Rakshith Raj <rakshithraj550@gmail.com>
2021-10-05 18:12:56 -0400

    Init data structures

--- modules/objects.py
+++ modules/objects.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 
 
 class Block:
@@ -10,7 +10,16 @@ class QuorumCertificate:
 
 
 class TimeoutCertificate:
-    pass
+
+    def __init__(
+        self,
+        round: int,
+        tmo_high_qc_rounds: List[int],
+        tmo_signatures: List[Any]
+    ) -> None:
+        self.round = round
+        self.tmo_high_qc_rounds = tmo_high_qc_rounds        
+        self.tmo_signatures = tmo_signatures        
 
 
 class TimeoutInfo:
@@ -31,12 +40,11 @@ class TimeoutInfo:
 
 
 class TimeoutMessage:
-    def __init__(self, round_no, high_qc, sender, signature) -> None:
-        self.round = round_no
-        self.high_qc = high_qc
-        self.sender = sender
-        self.signature = signature
 
+    def __init__(self, tmo_info: TimeoutInfo, last_round_tc: TimeoutCertificate, high_commit_qc: QuorumCertificate) -> None:
+        self.tmo_info = tmo_info
+        self.last_round_tc = last_round_tc
+        self.high_commit_qc = high_commit_qc
 
 class LedgerCommitInfo:
     
@@ -45,8 +53,8 @@ class LedgerCommitInfo:
         commit_state_id: int,
         vote_info_hash: str
     ) -> None:
-        commit_state_id: int  # ⊥ if no commit happens when this vote is aggregated to QC
-        vote_info_hash: str  # Hash of VoteMsg.vote info
+        self.commit_state_id = commit_state_id  # ⊥ if no commit happens when this vote is aggregated to QC
+        self.vote_info_hash = vote_info_hash  # Hash of VoteMsg.vote info
 
 
 class VoteInfo:
@@ -82,15 +90,26 @@ class VoteMsg:
         self.sender = sender
         self.signature = signature
         
+
+class ProposalMessage:
     
-    vote_info: VoteInfo  # A VoteInfo record
-    ledger_commit_info: LedgerCommitInfo  # Speculated ledger info
-    high_commit_qc: QuorumCertificate
-    # sender ← u; // Added automatically when constructed
-    # signature ← signu(ledger commit info); // Signed automatically when constructed
-    pass
+    def __init__(
+        self, 
+        block: Block, 
+        last_round_tc: TimeoutCertificate, 
+        high_commit_qc: QuorumCertificate,
+        signature: Any
+        ) -> None:
+        
+        self.block = block
+        self.last_round_tc = last_round_tc
+        self.high_commit_qc = high_commit_qc
+        self.signature = signature  # TODO: signu(block.id);
 
 class Certificate:
+
+    def __init__(self) -> None:
+        pass
 
     @staticmethod
     def is_valid_signatures(block: Block, certificate: TimeoutCertificate) -> bool:
