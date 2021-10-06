f7a91b9925dd3ce21bedf8fc9104e3e429423685
Akshay Somayaji <akshayafc1995@gmail.com>
2021-10-06 19:03:11 -0400

    Adding LeaderElection module base

---
+++ modules/leaderelection/leaderelection.py
@@ -0,0 +1,65 @@
+import random
+from typing import Any, Dict, List
+
+from modules.ledger.ledger import Ledger
+from modules.objects import QuorumCertificate
+from modules.pacemaker.pacemaker import Pacemaker
+
+
+class Leader:
+    pass
+
+
+class LeaderElection:
+    def __init__(
+        self,
+        validators: List[Leader],
+        window_size: int,
+        exclude_size: int,
+        reputation_leaders: Dict[int, Leader] = {},
+    ) -> None:
+        self.validators = validators
+        self.window_size = window_size
+        self.exclude_size = exclude_size
+        self.reputation_leaders = reputation_leaders
+
+    def elect_reputation_leader(self, qc: QuorumCertificate, ledger: Ledger) -> Leader:
+        active_validators, last_authors = set(), set()
+        current_qc = qc
+
+        i = 0
+        while i < self.window_size or len(last_authors) < self.exclude_size:
+            # TODO: Assumes below method in Ledger is implemented
+            current_block = ledger.committed_block(current_qc.vote_info.parent_id)
+            block_author = current_block.author
+
+            if i < self.window_size:
+                active_validators = active_validators | current_qc.signatures.signers()
+            if len(last_authors) < self.exclude_size:
+                last_authors = last_authors | set(block_author)
+
+            current_qc = current_block.qc
+            i += 1
+
+        active_validators = active_validators - last_authors
+        random.seed(qc.vote_info.round)
+        return random.choice(tuple(active_validators))
+
+    def update_leaders(
+        self, qc: QuorumCertificate, pacemaker: Pacemaker, ledger: Ledger
+    ):
+        extended_round = qc.vote_info.parent_round
+        qc_round = qc.vote_info.round
+        current_round = pacemaker.current_round
+
+        if extended_round + 1 == qc_round and qc_round + 1 == current_round:
+            self.reputation_leaders[current_round + 1] = self.elect_reputation_leader(
+                qc, ledger
+            )
+
+    def get_leader(self, round: int) -> Leader:
+        leader = self.reputation_leaders.get(round)
+        if leader:
+            return leader
+
+        return self.validators[(round // 2) % len(self.validators)]
